e1e43a2338a2905b29697f85790ee27ee532d641
K0o97 <rihood.sec@gmail.com>
2020-04-27 11:00:42 +0700

    Delete middleware

--- routes/exercise.route.js
+++ routes/exercise.route.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const exerciseController = require('../controllers/exercise.controller');
-const checkDayMiddleware = require('../middleware');
 
 const router = express.Router();
 
-router.post('/add', checkDayMiddleware.checkDay, exerciseController.postAdd);
+router.post('/add', exerciseController.postAdd);
 router.get('/log', exerciseController.getLog);
 
 module.exports = router;
